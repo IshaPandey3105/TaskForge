013ca2ee1a7cf3fdd52122d37d854d9840f8b40d
IshaPandey3105 <pandeyisha3105@gmail.com>
2026-06-22 15:12:50 +0530

    feat: implement file upload middleware with multer

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -2,17 +2,19 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images");
+    // This storage needs public/images folder in the root directory
+    // Else it will throw an error saying cannot find path public/images
+    cb(null, `./public/images`);
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
+// Middleware responsible to read form data and upload the File object to the mentioned path
 export const upload = multer({
-    storage: storage ,
-    limits:{
-        fileSize : 1* 1000 *1000,  
-    },
+  storage,
+  limits: {
+    fileSize: 1 * 1000 * 1000,
+  },
 });
